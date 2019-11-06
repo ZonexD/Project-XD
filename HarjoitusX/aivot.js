b65da350f22d7610c6f2c30bbfdc924db1bcc008
ZonexD <aavikkoeetu@gmail.com>
2019-11-06 11:55:05 +0200

    Mestariteos noob slayer päivitys
    Tänään hankittiin noobeille wallhack nappula

---
+++ HarjoitusX/aivot.js
@@ -0,0 +1,38 @@
+function laatikkoFunktio(boxNumber){
+    alert("Nyt vaihdetaan laatikko"+ boxNumber + ":n sisältöä!");
+
+    if (boxNumber == 1){
+        document.getElementById("laatikko1").innerHTML = "<h1 class='uusiOtsikko1'>Tähän tulee uusi otsikko!</h1>" + 
+         "<p>Ja tähän tietysti jotain sisältöä...</p>";
+        document.getElementById("laatikko1").style.lineHeight = "50px";
+    }
+    else if (boxNumber == 2){
+        document.getElementById("laatikko2").innerHTML = "<table>\
+        <tr>\
+           <td>Solu 1</td>\
+           <td>Solu 2</td>\
+        </tr>\
+     </table>";
+    }
+    else if (boxNumber == 3){
+        document.getElementById("laatikko3").innerHTML = "<img src='smiley2.png'>"
+    }
+    else{
+
+    }
+
+}
+function yhteystietoFunktio() {
+    var x = document.getElementById("yhteystietolomake");
+    var y = ["Nimi: " , "Email: " , "Puh: "];
+    var text = "";
+    var i;
+    for (i = 2; i >= 0; i--) {
+      text += y[i] + x.elements[i].value + "<br>";
+    }
+    document.getElementById("yhteystiedotTuloste").innerHTML = text;
+  }
+  function palautusFunktio(boxNumber){
+      alert("Palautetaan nettisivu alkuperäiseen tilaan napilla" + boxNumber);
+      document.location.reload();
+  }
